424aba847004c4bbca6188b0f45593c70d2fc1d0
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-07-22 14:33:43 +0200

    updates

--- Airport_FlightTracker1/MainWindow.xaml.cs
+++ Airport_FlightTracker1/MainWindow.xaml.cs
@@ -24,27 +24,27 @@ namespace Airport_FlightTracker1
         {
             InitializeComponent();
 
-            /* Creating a txt file in the application domain on load of the app */
+            /* sets the path to create the text file in the application domain on load of the app */
             filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Flight_Details.txt");
+            
         }
-
         /* A thread is like a worker inside your program that executes instructions one after another 
            Every program has atleast 1 thread (called the main thread) that runs the application 
            We can have multiple threads working at the same time, allowing our program to do several tasks simultaneously 
            Using multiple threads helps increase performance and keeps apps responsive */
-        private void btnBoarded_Click(object sender, RoutedEventArgs e) //Synchronous method
+        private void btnBoarded_Click(object sender, RoutedEventArgs e) //Synchronous method writes to file 
         {
             string name = txtPassengerName.Text.Trim();
             string seat = txtSeatNumber.Text.Trim();
-
             string flightInfoLine = $"{name},''{seat}, ''{DateTime.Now:yyyy-MM-dd HH:mm:ss}, Boarded";
 
             try
             {
-                // This is synchronous and will block the UI thread
+                // This is synchronous and will block the UI thread and write to the file created
                 File.AppendAllText(filePath, flightInfoLine + Environment.NewLine);
-                MessageBox.Show("Boarded info logged.");
+
                 Thread.Sleep(5000); // simulates the action when it takes long for stuff to be written in real life programs
+                MessageBox.Show("Boarded info logged.");
             }
             catch (Exception ex)
             {
@@ -58,13 +58,14 @@ namespace Airport_FlightTracker1
             string name = txtPassengerName.Text.Trim();
             string seat = txtSeatNumber.Text.Trim();
 
-            string line = $"{name},{seat},{DateTime.Now:yyyy-MM-dd HH:mm:ss},Boarded";
+            string line = $"{name},{seat},{DateTime.Now:yyyy-MM-dd HH:mm:ss}, Boarded";
 
             try
             {
                 /* * In real world when the writing to the file takes long the thread on which UI is working on will not freeze. 
                   */
-                await Task.Delay(3000);
+
+                await Task.Delay(5000);
 
                 // Asynchronously write without blocking UI thread
                 /* "await" means process is awaiting response from the other thread that is doing the other task (writing to the file)
